b8ee115713a43ac9898893049eb97dab06703c6e
DogyeomPark <qkrehfud@gmail.com>
2023-12-05 16:54:19 +0900

    배열과 문자열

--- Chapter11_01/Chapter11_01/Chapter11_01.c
+++ Chapter11_01/Chapter11_01/Chapter11_01.c
@@ -2,5 +2,19 @@
 
 int main()
 {
+	char str[50];
+	int idx = 0;
+
+	printf("문자열을 입력해주세요");
+	scanf_s("%s", str, 50);
+	printf("입력 받은 문자열: %s \n", str);
+
+	printf("문자 단위 출력:");
+	while (str[idx] != '\0')
+	{
+		printf("%c\t", str[idx]);
+		idx++;
+	}
+	printf("\n");
 	return 0;
 }
