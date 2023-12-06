143d4b3beb81baab0d1b9fdfa15cdda003838cdc
DogyeomPark <qkrehfud@gmail.com>
2023-12-06 09:54:16 +0900

    포인터 배열

--- Chapter13_03/Chapter13_03/Chapter13_03.c
+++ Chapter13_03/Chapter13_03/Chapter13_03.c
@@ -12,5 +12,15 @@ int main()
 	str1[0] = 'X';
 	/*str2[0] = 'X';*/
 	printf("%s\n%s\n", str1, str2);
+
+	int num1 = 10;
+	int num2 = 20;
+	int num3 = 30;
+
+	int* ptr1 = &num1;
+	int* ptr2 = &num2;
+	int* ptr3 = &num3;
+
+	int* arr[3];
 	return 0;
 }
